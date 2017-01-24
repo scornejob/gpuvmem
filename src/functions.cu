ebc52cb0fffe81237b523401b51763d7f6b820be
miguelcarcamov <miguel.carcamo@usach.cl>
2017-01-24 00:13:10 -0300

    fix error case tests

--- src/functions.cu
+++ src/functions.cu
@@ -661,8 +661,8 @@ __host__ Vars getOptions(int argc, char **argv) {
 	}
 
   if(variables.blockSizeX == -1 && variables.blockSizeY == -1 && variables.blockSizeV == -1 ||
-     strip(variables.input, " ") == "" && strip(variables.output, " ") == "" && strip(variables.output_image, " ") == "" && strip(variables.inputdat, " ") == "" ||
-     strip(variables.modin, " ") == "" && strip(variables.path, " ") == "") {
+     strcmp(strip(variables.input, " "),"")!=0 && strcmp(strip(variables.output, " "),"")!=0 && strcmp(strip(variables.output_image, " "),"")!=0 && strcmp(strip(variables.inputdat, " "),"")!=0 ||
+     strcmp(strip(variables.modin, " "),"")!=0 && strcmp(strip(variables.path, " "),"")!=0) {
         print_help();
         exit(EXIT_FAILURE);
   }
