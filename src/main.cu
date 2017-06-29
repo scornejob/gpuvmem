30a03e115bb18335b5c619a8cd3c2ae1338bfe0e
miguelcarcamov <miguel.carcamo@usach.cl>
2017-06-29 17:25:19 -0400

    not working

--- src/main.cu
+++ src/main.cu
@@ -682,14 +682,14 @@ __host__ int main(int argc, char **argv) {
 	float fret = 0.0;
 	frprmn(device_3I	, ftol, &fret, chiCuadrado, dchiCuadrado, 1);
   chiCuadrado(device_3I);
-  frprmn(device_3I	, ftol, &fret, chiCuadrado, dchiCuadrado, 0);
+  frprmn(device_3I	, ftol, &fret, chiCuadrado, dchiCuadrado, 2);
   chiCuadrado(device_3I);
   frprmn(device_3I	, ftol, &fret, chiCuadrado, dchiCuadrado, 1);
   chiCuadrado(device_3I);
-  frprmn(device_3I	, ftol, &fret, chiCuadrado, dchiCuadrado, 0);
-  chiCuadrado(device_3I);
   frprmn(device_3I	, ftol, &fret, chiCuadrado, dchiCuadrado, 2);
   chiCuadrado(device_3I);
+  /*frprmn(device_3I	, ftol, &fret, chiCuadrado, dchiCuadrado, 2);
+  chiCuadrado(device_3I);*/
   t = clock() - t;
   end = omp_get_wtime();
   printf("Minimization ended successfully\n\n");
