35eb90c2cc96ce4f27021fa921cb7e46adda4006
miguelcarcamov <miguel.carcamo@usach.cl>
2017-08-09 10:52:09 -0400

    my changes

--- src/main.cu
+++ src/main.cu
@@ -712,10 +712,10 @@ __host__ int main(int argc, char **argv) {
   //gpuErrchk(cudaDeviceSynchronize());
 	frprmn(device_3I, ftol, &fret, chiCuadrado, dchiCuadrado, 0);
   chiCuadrado(device_3I);
-  frprmn(device_3I, ftol, &fret, chiCuadrado, dchiCuadrado, 1);
+  /*frprmn(device_3I, ftol, &fret, chiCuadrado, dchiCuadrado, 1);
   chiCuadrado(device_3I);
   frprmn(device_3I, ftol, &fret, chiCuadrado, dchiCuadrado, 0);
-  chiCuadrado(device_3I);
+  chiCuadrado(device_3I);*/
   frprmn(device_3I, ftol, &fret, chiCuadrado, dchiCuadrado, 2);
   chiCuadrado(device_3I);
   t = clock() - t;
