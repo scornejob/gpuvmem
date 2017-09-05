334fecb6faef4842a55f4c1621d778f173fd1e04
miguelcarcamov <miguel.carcamo@usach.cl>
2017-09-05 18:13:47 -0300

    fix reading

--- src/MSFITSIO.cu
+++ src/MSFITSIO.cu
@@ -527,7 +527,7 @@ __host__ void writeMS(char *infile, char *outfile, Field *fields, freqData data,
           if(field == f && spw == i && flag == false){
             for (int sto=0; sto< data.nstokes; sto++){
               auxbool = flagCol[j][sto];
-              if(auxbool[0] == false){
+              if(auxbool[0] == false && weights[sto] != 0.0){
                 comp.real() = fields[f].visibilities[g].Vo[h].x - fields[f].visibilities[g].Vm[h].x;
                 comp.imag() = fields[f].visibilities[g].Vo[h].y - fields[f].visibilities[g].Vm[h].y;
                 dataCol[j][sto] = comp;
