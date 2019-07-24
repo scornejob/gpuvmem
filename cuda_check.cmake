cf0c57b2d46c2292d35b154d2a526182a5eaa9f1
Miguel Carcamo <miguel.carcamo@protonmail.com>
2019-07-24 17:17:51 +0100

    CMakeLists.txt added

---
+++ cuda_check.cmake
@@ -0,0 +1,33 @@
+# Find CUDA
+find_package(CUDA)
+
+if (CUDA_FOUND)
+  #Get CUDA compute capability
+  set(OUTPUTFILE ${CMAKE_CURRENT_SOURCE_DIR}/scripts/cuda_script) # No suffix required
+  set(CUDAFILE ${CMAKE_CURRENT_SOURCE_DIR}/scripts/check_cuda.cu)
+  execute_process(COMMAND nvcc -lcuda ${CUDAFILE} -o ${OUTPUTFILE})
+  execute_process(COMMAND ${OUTPUTFILE}
+                  RESULT_VARIABLE CUDA_RETURN_CODE
+                  OUTPUT_VARIABLE ARCH)
+  execute_process(COMMAND rm ${OUTPUTFILE})
+                  
+  if(${CUDA_RETURN_CODE} EQUAL 0)
+    set(CUDA_SUCCESS "TRUE")
+  else()
+    set(CUDA_SUCCESS "FALSE")
+  endif()
+
+  if (${CUDA_SUCCESS})
+    message(STATUS "CUDA Architecture: ${ARCH}")
+    message(STATUS "CUDA Version: ${CUDA_VERSION_STRING}")
+    message(STATUS "CUDA Path: ${CUDA_TOOLKIT_ROOT_DIR}")
+    message(STATUS "CUDA Libararies: ${CUDA_LIBRARIES}")
+    message(STATUS "CUDA Performance Primitives: ${CUDA_npp_LIBRARY}")
+
+    set(CUDA_NVCC_FLAGS "${ARCH}")
+    add_definitions(-DGPU) #You may not require this
+
+  else()
+    message(WARNING ${ARCH})
+  endif()
+endif()
